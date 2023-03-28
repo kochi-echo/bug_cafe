3c827aca07c18ed8ea1ea99dae9877630a18ef85
kochi-echo <tnodtegrof@gmail.com>
2023-03-28 12:35:45 +0900

    getsを0ベースに変更、totalの計算元をIntクラスに変更しDRINKSとFOODSを入れ替えた

--- cafe.rb
+++ cafe.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
+require 'debug'
+
+=begin
+修正内容：
+- 頼んだつもりのメニューが選択できない。
+  -> 一つ目の注文で1個後のが選択される
+  -> １ベースになっているか？
+- 最後の番号のメニューを選択するとエラーとなる。
+  ->最後の注文のarrayがnil
+  ->上記の原因と同じだと思われる。
+- お会計額が異様に高い。
+  ->金額が合計ではない
+- 違う番号のドリンク、フードを注文したとき、お会計が正しくない
+方針：
+- メニューの選択のところを確認し、0ベースにする
+- 金額を+に変更
+=end
+
 
 DRINKS = [
   { name: 'コーヒー', price: '300' },
@@ -18,8 +36,11 @@ def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
-  print '>'
-  order_number = gets.to_i
+  order_number = -1
+  while order_number < 0 || order_number >= menus.size
+    print '>'
+    order_number = gets.to_i - 1
+  end
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
@@ -30,5 +51,5 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
