9ee066a7dbee46b297e06813e73ff3d654c3812e
kochi-echo <tnodtegrof@gmail.com>
2023-04-02 09:10:30 +0900

    オーダーのindexの名前を意味が伝わるように修正、範囲外の時に再入力を明記、qで注文中止する処理を追加

--- cafe.rb
+++ cafe.rb
@@ -14,24 +14,37 @@ FOODS = [
   { name: 'ホットサンド', price: '410' }
 ].freeze
 
+def input_order_number(menus)
+  order_number = nil
+  loop do
+    print '>'
+    order_str = gets
+    raise "注文を中止します。またのご利用お待ちしております。" if order_str == "q\n"
+    order_number = order_str.to_i - 1
+    break if order_number >= 0 && order_number < menus.size
+    puts '範囲外です。もう一度番号でご注文ください。(注文を中止する場合はqを押してください。)'
+  end
+  order_number
+end
+
 def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
-  order_number = -1
-  while order_number < 0 || order_number >= menus.size
-    print '>'
-    order_number = gets.to_i - 1
-  end
+  order_number = input_order_number(menus)
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
 
-puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+begin
+  puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ。(注文を中止する場合はqを押してください。)'
+  order_drink = take_order(DRINKS)
 
-puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+  puts 'フードメニューはいかがですか?'
+  order_food = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
-puts "お会計は#{total}円になります。ありがとうございました！"
+  total = DRINKS[order_drink][:price].to_i + FOODS[order_food][:price].to_i
+  puts "お会計は#{total}円になります。ありがとうございました！"
+rescue => error_massage
+  puts error_massage
+end
